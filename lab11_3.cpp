c21d3dd3da75065cd56e13371ad4aa93133dddd9
Nuttadon <nuttadon_p@cmu.ac.th>
2021-12-17 00:33:15 +0700

    lab11_3 complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    ifstream score("score.txt");
+    string box;
+    float mean,sum=0,rpwsum=0,sd;
+    int n=0;
+    while(getline(score,box)){
+        sum += stod(box);
+        rpwsum += stod(box)*stod(box);
+            n++;
+    }
+    mean = sum/n;
+    sd = sqrt((rpwsum/n)-(mean*mean));
+
+cout << "Number of data = "<<n<<endl;
+cout << setprecision(n%10); 
+cout << "Mean = "<<mean<<endl;
+cout << "Standard deviation = "<<sd;
+}
